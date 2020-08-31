21a93d3505cc2462cffa2d82a676f1af9b63ab27
Daniel Fuzari <daniel.fuzari@i-value.com.br>
2020-08-31 19:30:33 -0300

    feat(controllers): remove password from response object

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -9,7 +9,9 @@ router.post('/register', async(req, res) => {
         if(await User.findOne({ email }))
             return res.status(400).send({ error: 'User already exists'});
 
-        const user = await User.create(req.body)
+        const user = await User.create(req.body);
+        user.password = undefined;
+
         return res.send({user})
     } catch(err) {
         return res.status(400).send({error: 'Registration failed'});
