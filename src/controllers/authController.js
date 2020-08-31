b368b25f2956e3dcb70bcf35047bcc8bb56fd230
Daniel Fuzari <daniel.fuzari@i-value.com.br>
2020-08-31 19:29:08 -0300

    feat(controllers): check if user already exists before create it

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,7 +4,11 @@ const User = require('../models/user');
 const router = express.Router();
 
 router.post('/register', async(req, res) => {
+    const { email } = req.body;
     try {
+        if(await User.findOne({ email }))
+            return res.status(400).send({ error: 'User already exists'});
+
         const user = await User.create(req.body)
         return res.send({user})
     } catch(err) {
