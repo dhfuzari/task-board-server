e2616adb193742cce32085e910c3acb9502b1800
Daniel Fuzari <daniel.fuzari@i-value.com.br>
2020-08-31 19:28:06 -0300

    feat(models): encrypt password field before save it in the database

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('../database/index');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -21,6 +22,12 @@ const UserSchema = new mongoose.Schema({
         default: Date.now
     }
 });
+
+UserSchema.pre('save', async function(next) {
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+    next();
+})
 
 const User = mongoose.model('User', UserSchema);
 
