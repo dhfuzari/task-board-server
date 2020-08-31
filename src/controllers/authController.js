e3cc05b304a0c958802375a876a3158fb774a85a
Daniel Fuzari <daniel.fuzari@i-value.com.br>
2020-08-31 19:07:23 -0300

    feat(controllers): returns created user

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const router = express.Router();
-
 const User = require('../models/user');
+
+const router = express.Router();
 
 router.post('/register', async(req, res) => {
     try {
         const user = await User.create(req.body)
-        return res.send({})
+        return res.send({user})
     } catch(err) {
         return res.status(400).send({error: 'Registration failed'});
     }
