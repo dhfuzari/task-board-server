2a9f618712a60a6eb85cb01f08132885059fb61a
Daniel Fuzari <daniel.fuzari@i-value.com.br>
2020-09-01 08:55:45 -0300

    feat(controllers): authenticate route created to validate user

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
 const router = express.Router();
@@ -17,6 +18,22 @@ router.post('/register', async(req, res) => {
         return res.status(400).send({error: 'Registration failed'});
     }
 });
+
+router.post('/authenticate', async(req, res) => {
+    const { email, password } = req.body;
+
+    // Get email and password from mongoose. '+password' ask to mongoose get 
+    // password key/value from mongodb, wich is selected as select:false'
+    const user = await User.findOne({ email }).select('+password');
+
+    if(!user) 
+        return res.status(400).send({error: 'User not found'});
+    if(!await bcrypt.compare(password, user.password)) 
+        return res.status(400).send({error: 'Invalid password'});
+    
+    user.password = undefined;
+    return res.status(200).send({ user }); 
+})
 
 module.exports = app => app.use('/auth', router);
 
