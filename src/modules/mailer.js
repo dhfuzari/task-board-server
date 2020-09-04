18ce51d9288ec17ba884e1f69883ad5ac15d26e7
Daniel Fuzari <daniel.fuzari@i-value.com.br>
2020-09-04 15:55:54 -0300

    feat(modules): config nodemailer to use handlebars templates

--- src/modules/mailer.js
+++ src/modules/mailer.js
@@ -1,10 +1,18 @@
 const nodemailer = require('nodemailer');
+const path = require('path')
+const hbs = require('nodemailer-express-handlebars');
 const { host, port, user, pass } = require('../config/mailtrapServer.json');
 
 const transport = nodemailer.createTransport({
     host,
     port,
     auth: { user, pass }
-  });
+});
 
-  module.exports = transport;
+transport.use('compile', hbs({
+    viewEngine: 'handlebars',
+    viewPath: path.resolve('./src/resources/mail/'),
+    extName: '.html'
+}))
+
+module.exports = transport;
