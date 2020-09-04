655d733ff000cddbae300ce343ef88cc06fd1d00
Daniel Fuzari <daniel.fuzari@i-value.com.br>
2020-09-04 12:18:53 -0300

    feat(controllers): add controllers files automatically to index.js file

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
-require('./app/controllers/authController')(app);
-require('./app/controllers/projectController')(app);
+require('./app/controllers/index')(app);
 
 app.listen(3000);
