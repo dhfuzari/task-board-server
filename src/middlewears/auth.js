3cf5b7669b9860d9bfed1644160d81e2fba1c3c0
Daniel Fuzari <daniel.fuzari@i-value.com.br>
2020-09-04 09:32:40 -0300

    feat(middlewares): authetication middleware added to validate private routes

---
+++ src/middlewears/auth.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+const authConfig = require("../config/auth.json")
+
+module.exports = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader) // Check if Authorization header exists
+        return res.status(401).send({ error: 'No token provided' });
+    
+    const parts = authHeader.split(' ');
+
+    if(!parts.length === 2) // Check if token has two parts(scheme and hash)
+        return res.status(401).send({ error: 'Token schema error'});
+
+    const[scheme, token] = parts;
+
+    if(!/^Bearer$/i.test(scheme)) // Check if scheme has the string "Bearer"
+        return res.status(401).send({ error: 'Token malformatted' })
+
+    jwt.verify(token, authConfig.secret, (err, decoded) => {
+        if(err) return res.status(401).send({ error: 'Invalid token'});
+
+        req.userId = decoded.id;
+        return next();
+    })
+}
