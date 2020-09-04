56d8071044cb962808c965ca6b01430eb7161b86
Daniel Fuzari <daniel.fuzari@i-value.com.br>
2020-09-04 12:00:32 -0300

    refactor(app) put application files in a commun folder

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -1,4 +1,4 @@
-const mongoose = require('../database/index');
+const mongoose = require('../../database/index');
 const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { request } = require('express');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
-require('./controllers/authController')(app);
-require('./controllers/projectController')(app);
+require('./app/controllers/authController')(app);
+require('./app/controllers/projectController')(app);
 
 app.listen(3000);
