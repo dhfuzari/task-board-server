50e185868dd8547b3c06b1ebedba0a27a4622f61
Daniel Fuzari <daniel.fuzari@i-value.com.br>
2020-09-06 11:41:59 -0300

    feat(controllers): created forgot password function

--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const authConfig = require('../../config/auth.json')
+const crypto = require('crypto');
+const authConfig = require('../../config/auth.json');
+const mailer = require('../../modules/mailer');
 const User = require('../models/user');
 
 const router = express.Router();
@@ -48,6 +50,45 @@ router.post('/authenticate', async(req, res) => {
     const token = generateToken({id: user.id});
 
     return res.status(200).send({ user, token }); 
+});
+
+router.post('/forgot_password', async (req, res) => {
+    const { email } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+        if(!user)
+            return res.status(400).send({error: 'User not found'});
+        
+        const token = crypto.randomBytes(20).toString('hex');
+        const now = new Date();
+        now.setHours(now.getHours() + 1);
+
+        console.log(token, now)
+
+        await User.findByIdAndUpdate(user.id, {
+            '$set': {
+                passwordResetToken: token,
+                passwordResetExpires: now
+            }
+        });
+
+        mailer.sendMail({
+            from: '"api-nodejs-express-mongo 👻" <foo@example.com>',
+            to: email,
+            subject: "Hello ✔",
+            template: 'auth/forgot-password',
+            context: { token }
+        }, (err) => {
+            if(err) { 
+                console.log(err); res.status(400).send({ error: 'Cannot send forgot email password'}) 
+            } else { 
+                res.status(200).send({ message: 'Email successfuly send' })
+            }
+        });
+    } catch(err) {
+        res.status(400).send({ error: 'Error on forgot password, try again later' });
+    }
 })
 
 module.exports = app => app.use('/auth', router);
