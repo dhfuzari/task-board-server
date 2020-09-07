0ff478b394825a9f823e73ba3011e914601b8b8d
Daniel Fuzari <daniel.fuzari@i-value.com.br>
2020-09-06 22:55:40 -0300

    feat(controllers): project controller routes defined

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const authMiddleware = require('../middlewears/auth');
 
+const Project = require('../models/project');
+const Task = require('../models/task');
+
 const router = express.Router();
 router.use(authMiddleware);
 
-router.get('/', (req, res) => {
-    res.status(200).send({ authenticated: true, userId: req.userId });
+router.get('/', async (req, res) => {
+    res.status(200).send({ userId: req.userId });
+});
+
+router.get('/:projectId', async (req, res) => {
+    res.status(200).send({ userId: req.userId });
+});
+
+router.post('/', async (req, res) => {
+    res.status(200).send({ userId: req.userId })
+})
+
+router.put('/:projectId', async (req, res) => {
+    res.status(200).send({ userId: req.userId });
+});
+
+router.delete('/:projectId', async (req, res) => {
+    res.status(200).send({ userId: req.userId });
 });
 
 module.exports = app => app.use('/projects', router);
